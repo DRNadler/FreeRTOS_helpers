eacdaebdb93a2019ea840da87ab7a6966fc6b08f
Dave Nadler <drn@nadler.com>
2020-06-24 20:30:13 -0400

    Cleaned up and consolidated FreeRTOS helpers from assorted customer projects.

---
+++ README.md
@@ -0,0 +1,44 @@
+# FreeRTOS Helpers
+We see lots of vendors, application developers, and a few consultants Pontificating and Spewing about how solid their tools and applications are, citing MISRA-compliance and other bullox. If they would shut up for a moment and glance downwards, they'd notice they are wearing no pants and standing in quicksand, in the middle of a swamp surrounded by crocodiles...
+
+**If you're using FreeRTOS and GCC, I hope the tools in this repository can help you create a stable platform for your applications using FreeRTOS**, at least a bit... After all, as my wife sometimes reminds it is better to wear pants, and really best to avoid standing in quicksand amidst crocodiles.
+
+## Thoughts about FreeRTOS
+We've delivered many successful applications using FreeRTOS (more than a dozen for sure but truly I've lost count). This on MCUs like PIC24F, PIC32MX, TI MSP430, and ARM Cortex from M0 through M7 from Freescale, NXP, Silicon Labs, etc. FreeRTOS supports a number of compilers, though here we use mostly GCC. We are forever in debt to Richard Barry for his most excellent efforts (and glad I was able to help a bit with the Cortex-M0 port). So, what's not to like?
+
+FreeRTOS provides a truly excellent OS, with pretty much all the features you need for non-trivial applications on MCUs (single-core, anyway). What **FreeRTOS does NOT provide** is integration with the MCU vendor toolchains; that's left to the MCU vendors. Most importantly for typical embedded applications, FreeRTOS does not provide:
+* device drivers (ethernet, serial, USB, etc.)
+* USB Stack
+* low-level memory management (sbrk level for many toolchains)
+* integration with C-RTL (FreeRTOS assumes a working C toolchain)
+* LwIP (recently documented on FreeRTOS site as more performant than FreeRTOS TCP/IP)
+
+As the vendor-provided bits are usually crap, it can often take more time/money to create a working basic platform  than the actual target application. Not a great state of affairs. And as the complexity of the MCU peripherals (and required drivers) continues to increase, the situation worsens.
+
+## We Support FreeRTOS! Yeah! Woo Hoo!
+
+Well, so the MCU vendors loudly lie. The reality is horrendous. If you actually need to build an application that is both performant and reliable, the stuff provided by MCU vendors is often completely unworkable. Typical fuckups include:
+* Memory management is completely broken. Your application will crash or behave mysteriously and corrupt memory when you try to use sprintf, dtoa, strtok, or other commonly-utilitized C-RTL functions. See the Heap_useNewlib section below for a solution to this one (especially if you're trying to use ST MCUs).
+* drivers do not use FreeRTOS facilities. For performance and interrupt latency, we expect and **require** minimal code in ISRs moving data to/from higher layers via RTOS facilities (see https://www.freertos.org/deferred_interrupt_processing.html for a great explanation). Instead we see crap code like:
+-- ST: Incomplete, buggy, and horrendously coded USB stack executed almost entirely within ISR, even using malloc inside ISR! Incomplete USB functionality, inadequate performance, and hugely excessive interrupt latency. As an added bonus, drops USB packets under load.
+-- ST: Buggy ethernet drivers: Missing required memory barriers, not properly coded for context-safety (ie race conditions between ISR code and foreground code).
+-- ST: Improper LwIP integration
+-- ST: Crypto hardware, but no drivers integrated into TCP/IP to support HTTPS etc.
+-- Microchip: buggy drivers polled in a single timer-based thread not properly integrated with FreeRTOS. You probably won't notice this if you try to use Microchip's Harmony framework, as most likely the output of their 'configuration wizard' won't even compile!
+-- Silicon Labs: USB driver incomplete, buggy, and not integrated with FreeRTOS.
+
+I could go on but it gives me a headache. Again, the consequence of this sad state of affairs is it often costs more time/money to get a working and performant platform than to create your actual application. You get to rewrite and stress-test drivers and their RTOS integration for your chosen peripherals, and after that maybe you can develop your application...
+
+# Safe memory management: heap_useNewlib
+[GNU ARM Embedded Toolchain distributions](https://developer.arm.com/open-source/gnu-toolchain/gnu-rm) include a non-polluting reduced-size runtime library called newlib (or newlib-nano for the smallest variant). If you use an MCU-vendor-distributed toolchain you're typically using these tools. Unfortunately, newlib internally uses free storage (malloc/free) in startling places within the C runtime library. Thus, newlib free storage routines get dragged in and used unexpectedly. And some MCU vendors don't properly support newlib use within FreeRTOS. So your application will crash or behave mysteriously and corrupt memory when you try to use sprintf, dtoa, strtok, or other commonly-utilitized C-RTL functions. Please see my web pages for details about the problem and how to use the solution (code) provided in this repository:
+* [NXP MCUXpresso users](http://www.nadler.com/embedded/NXP_newlibAndFreeRTOS.html)
+* [ST RubeMX users](http://www.nadler.com/embedded/NXP_newlibAndFreeRTOS.html)
+* Other MCUs and toolchains: start with the NXP version
+
+# FreeRTOS ISR Stack Check (for Arm Cortex M4-7)
+On ARM, FreeRTOS ISRs run on the dedicated MSP stack, typically allocated at top of RAM. This is great, as you don't need to reserve stack space for the deepest nested ISR stack-use for each task as older processors. While FreeRTOS provides great tools for checking actual stack use for tasks, it does not provide any means for checking actual MSP stack use by your ISRs. port_DRN.C adds code to check MSP stack use. Eclude FreeRTOS normal port.c from your build and add this one. Configure your MSP stack size and enable MSP checking by adding to your FreeRTOSconfig.h:
+
+    // DRN ISR (MSP) stack initialization and checking
+    #define configISR_STACK_SIZE_WORDS (0x100) // in WORDS, must be valid constant for assembler
+    #define configSUPPORT_ISR_STACK_CHECK  1   // DRN initialize and check ISR stack
+    UBaseType_t xUnusedISRstackWords( void );  // check unused amount at runtime
---
+++ heap_useNewlib_NXP.c
@@ -0,0 +1,236 @@
+/**
+ * \file heap_useNewlib.c
+ * \brief Wrappers required to use newlib malloc-family within FreeRTOS.
+ *
+ * \par Overview
+ * Route FreeRTOS memory management functions to newlib's malloc family.
+ * Thus newlib and FreeRTOS share memory-management routines and memory pool,
+ * and all newlib's internal memory-management requirements are supported.
+ *
+ * \author Dave Nadler
+ * \date 22-July-2017
+ * \version 24-Jun-2020 commentary only
+ * \version 11-Sep-2019 malloc accounting, comments, newlib version check
+ *
+ * \see http://www.nadler.com/embedded/newlibAndFreeRTOS.html
+ * \see https://sourceware.org/newlib/libc.html#Reentrancy
+ * \see https://sourceware.org/newlib/libc.html#malloc
+ * \see https://sourceware.org/newlib/libc.html#index-_005f_005fenv_005flock
+ * \see https://sourceware.org/newlib/libc.html#index-_005f_005fmalloc_005flock
+ * \see https://sourceforge.net/p/freertos/feature-requests/72/
+ * \see http://www.billgatliff.com/newlib.html
+ * \see http://wiki.osdev.org/Porting_Newlib
+ * \see http://www.embecosm.com/appnotes/ean9/ean9-howto-newlib-1.0.html
+ *
+ *
+ * \copyright
+ * (c) Dave Nadler 2017-2020, All Rights Reserved.
+ * Web:         http://www.nadler.com
+ * email:       drn@nadler.com
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted provided that the following conditions are met:
+ *
+ * - Use or redistributions of source code must retain the above copyright notice,
+ *   this list of conditions, and the following disclaimer.
+ *
+ * - Use or redistributions of source code must retain ALL ORIGINAL COMMENTS, AND
+ *   ANY CHANGES MUST BE DOCUMENTED, INCLUDING:
+ *   - Reason for change (purpose)
+ *   - Functional change
+ *   - Date and author contact
+ *
+ * - Redistributions in binary form must reproduce the above copyright notice, this
+ *   list of conditions and the following disclaimer in the documentation and/or
+ *   other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ * ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include <stdlib.h> // maps to newlib...
+#include <malloc.h> // mallinfo...
+#include <errno.h>  // ENOMEM
+#include <stdbool.h>
+#include <stddef.h>
+
+#include "newlib.h"
+#if ((__NEWLIB__ == 2) && (__NEWLIB_MINOR__ < 5)) ||((__NEWLIB__ == 3) && (__NEWLIB_MINOR__ > 1))
+  #warning "This wrapper was verified for newlib versions 2.5 - 3.1; please ensure newlib's external requirements for malloc-family are unchanged!"
+#endif
+
+#include "freeRTOS.h" // defines public interface we're implementing here
+#if !defined(configUSE_NEWLIB_REENTRANT) ||  (configUSE_NEWLIB_REENTRANT!=1)
+  #warning "#define configUSE_NEWLIB_REENTRANT 1 // Required for thread-safety of newlib sprintf, dtoa, strtok, etc..."
+  // If you're *REALLY* sure you don't need FreeRTOS's newlib reentrancy support, comment out the above warning...
+#endif
+#include "task.h"
+
+// ================================================================================================
+// External routines required by newlib's malloc (sbrk/_sbrk, __malloc_lock/unlock)
+// ================================================================================================
+
+// Simplistic sbrk implementations assume stack grows downwards from top of memory,
+// and heap grows upwards starting just after BSS.
+// FreeRTOS normally allocates task stacks from a pool placed within BSS or DATA.
+// Thus within a FreeRTOS task, stack pointer is always below end of BSS.
+// When using this module, stacks are allocated from malloc pool, still always prior
+// current unused heap area...
+
+// Doesn't work with FreeRTOS: suggested minimal implementation from https://sourceware.org/newlib/libc.html#Syscalls:
+#if 0
+    // sbrk: Increase program data space. As malloc and related functions depend on this,
+    // it is useful to have a working implementation. The following suffices for a standalone system;
+    // it exploits the symbol _end automatically defined by the GNU linker.
+    caddr_t sbrk(int incr) {
+      extern char _end;     /* Defined by the linker */
+      static char *heap_end;
+      char *prev_heap_end;
+      if (heap_end == 0) {
+        heap_end = &_end;
+      }
+      prev_heap_end = heap_end;
+      if (heap_end + incr > stack_ptr) { //
+        write (1, "Heap and stack collision\n", 25);
+        abort ();
+      }
+      heap_end += incr;
+      return (caddr_t) prev_heap_end;
+    }
+#endif
+// Doesn't work with FreeRTOS: Freescale implementation
+#if 0
+    caddr_t _sbrk(int incr)
+    {
+        extern char end __asm("end");
+        extern char heap_limit __asm("__HeapLimit");
+        static char *heap_end;
+        char *prev_heap_end;
+        if (heap_end == NULL)
+            heap_end = &end;
+        prev_heap_end = heap_end;
+        if (heap_end + incr > &heap_limit) // of course, always true for FreeRTOS task stacks
+        {
+            errno = ENOMEM; // ...so first call inside a FreeRTOS task lands here
+            return (caddr_t)-1;
+        }
+        heap_end += incr;
+        return (caddr_t)prev_heap_end;
+    }
+#endif
+
+#ifndef NDEBUG
+    static int totalBytesProvidedBySBRK = 0;
+#endif
+extern char __HeapBase, __HeapLimit, HEAP_SIZE;  // make sure to define these symbols in linker command file
+static int heapBytesRemaining = (int)&HEAP_SIZE; // that's (&__HeapLimit)-(&__HeapBase)
+
+// Use of vTaskSuspendAll() in _sbrk_r() is normally redundant, as newlib malloc family routines call
+// __malloc_lock before calling _sbrk_r(). Note vTaskSuspendAll/xTaskResumeAll support nesting.
+
+//! _sbrk_r version supporting reentrant newlib (depends upon above symbols defined by linker control file).
+void * _sbrk_r(struct _reent *pReent, int incr) {
+    static char *currentHeapEnd = &__HeapBase;
+    vTaskSuspendAll(); // Note: safe to use before FreeRTOS scheduler started, but not within an ISR
+    if (currentHeapEnd + incr > &__HeapLimit) {
+        // Ooops, no more memory available...
+        #if( configUSE_MALLOC_FAILED_HOOK == 1 )
+        {
+            extern void vApplicationMallocFailedHook( void );
+            vApplicationMallocFailedHook();
+        }
+        #elif defined(configHARD_STOP_ON_MALLOC_FAILURE)
+            // If you want to alert debugger or halt...
+            while(1) { __asm("bkpt #0"); }; // Stop in GUI as if at a breakpoint (if debugging, otherwise loop forever)
+        #else
+            // Default, if you prefer to believe your application will gracefully trap out-of-memory...
+            pReent->_errno = ENOMEM; // newlib's thread-specific errno
+            xTaskResumeAll();  // Note: safe to use before FreeRTOS scheduler started, but not within an ISR;
+        #endif
+        return (char *)-1; // the malloc-family routine that called sbrk will return 0
+    }
+    // 'incr' of memory is available: update accounting and return it.
+    char *previousHeapEnd = currentHeapEnd;
+    currentHeapEnd += incr;
+    heapBytesRemaining -= incr;
+    #ifndef NDEBUG
+        totalBytesProvidedBySBRK += incr;
+    #endif
+    xTaskResumeAll();  // Note: safe to use before FreeRTOS scheduler started, but not within an ISR
+    return (char *) previousHeapEnd;
+}
+//! non-reentrant sbrk uses is actually reentrant by using current context
+// ... because the current _reent structure is pointed to by global _impure_ptr
+char * sbrk(int incr) { return _sbrk_r(_impure_ptr, incr); }
+//! _sbrk is a synonym for sbrk.
+char * _sbrk(int incr) { return sbrk(incr); };
+
+void __malloc_lock(struct _reent *p)   { configASSERT( !xPortIsInsideInterrupt() ); // Make damn sure no mallocs inside ISRs!!
+                                               vTaskSuspendAll(); };
+void __malloc_unlock(struct _reent *p) { (void)xTaskResumeAll();  };
+
+// newlib also requires implementing locks for the application's environment memory space,
+// accessed by newlib's setenv() and getenv() functions.
+// As these are trivial functions, momentarily suspend task switching (rather than semaphore).
+// Not required (and trimmed by linker) in applications not using environment variables.
+// ToDo: Move __env_lock/unlock to a separate newlib helper file.
+void __env_lock()    {       vTaskSuspendAll(); };
+void __env_unlock()  { (void)xTaskResumeAll();  };
+
+#if 1 // Provide malloc debug and accounting wrappers
+  /// /brief  Wrap malloc/malloc_r to help debug who requests memory and why.
+  /// To use these, add linker options: -Xlinker --wrap=malloc -Xlinker --wrap=_malloc_r
+  // Note: These functions are normally unused and stripped by linker.
+  size_t TotalMallocdBytes;
+  int MallocCallCnt;
+  static bool inside_malloc;
+  void *__wrap_malloc(size_t nbytes) {
+    extern void * __real_malloc(size_t nbytes);
+    MallocCallCnt++;
+    TotalMallocdBytes += nbytes;
+    inside_malloc = true;
+      void *p = __real_malloc(nbytes); // will call malloc_r...
+    inside_malloc = false;
+    return p;
+  };
+  void *__wrap__malloc_r(void *reent, size_t nbytes) {
+    extern void * __real__malloc_r(size_t nbytes);
+    if(!inside_malloc) {
+      MallocCallCnt++;
+      TotalMallocdBytes += nbytes;
+    };
+    void *p = __real__malloc_r(nbytes);
+    return p;
+  };
+#endif
+
+// ================================================================================================
+// Implement FreeRTOS's memory API using newlib-provided malloc family.
+// ================================================================================================
+
+void *pvPortMalloc( size_t xSize ) PRIVILEGED_FUNCTION {
+    void *p = malloc(xSize);
+    return p;
+}
+void vPortFree( void *pv ) PRIVILEGED_FUNCTION {
+    free(pv);
+};
+
+size_t xPortGetFreeHeapSize( void ) PRIVILEGED_FUNCTION {
+    struct mallinfo mi = mallinfo(); // available space now managed by newlib
+    return mi.fordblks + heapBytesRemaining; // plus space not yet handed to newlib by sbrk
+}
+
+// GetMinimumEverFree is not available in newlib's malloc implementation.
+// So, no implementation is provided: size_t xPortGetMinimumEverFreeHeapSize( void ) PRIVILEGED_FUNCTION;
+
+//! No implementation needed, but stub provided in case application already calls vPortInitialiseBlocks
+void vPortInitialiseBlocks( void ) PRIVILEGED_FUNCTION {};
